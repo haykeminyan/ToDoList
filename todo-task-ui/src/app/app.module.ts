2ceb68f36fc12164fadb84c8bdd59136519c80f8
Hayk Eminyan <43892880+haykeminyan@users.noreply.github.com>
2023-08-04 17:20:32 +0400

    version 0.0.1 small preparing for xakaton

--- todo-task-ui/src/app/app.module.ts
+++ todo-task-ui/src/app/app.module.ts
@@ -2,14 +2,21 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { TaskComponent } from './task/task.component';
+import {RouterOutlet} from "@angular/router";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
-  imports: [
-    BrowserModule
-  ],
+    imports: [
+        BrowserModule,
+        RouterOutlet,
+        HttpClientModule,
+
+    ],
   providers: [],
   bootstrap: [AppComponent]
 })
